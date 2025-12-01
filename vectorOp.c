91249d341ccea0eb391e5270d831416e26149153
AngusHsu <80775003h@ntnu.edu.tw>
2025-12-02 06:59:26 +0800

    VectorAdd(tested)

---
+++ vectorOp.c
@@ -0,0 +1,13 @@
+#include"vectorOp.h"
+#include <stdlib.h>
+int* vAdd(int *A,int *B,int size)
+{
+    int *C=malloc(sizeof(int)*size);
+    int i=0;
+    for(i=0;i<size;i++)
+    {
+        C[i]=A[i]+B[i];
+    }
+    return C;
+
+}
---
+++ vectorOp.h
@@ -0,0 +1,12 @@
+#ifndef VECTOROP_H
+#define VECTOROP_H
+
+int* vAdd(int *A,int *B,int size); 
+int* vSub(int *A,int *B,int size); 
+int* vDot(int *A,int *B,int size); 
+
+
+
+
+
+#endif
