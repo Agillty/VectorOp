32755abf13a4811fd0508279369318d013bf96a0
AngusHsu <80775003h@ntnu.edu.tw>
2025-12-02 11:56:01 +0800

    test3

--- vectorOp.c
+++ vectorOp.c
@@ -6,10 +6,11 @@ int* vAdd(int *A,int *B,int size)
     int i=0;
     for(i=0;i<size;i++)
     {
+        
         C[i]=A[i]+B[i];
     }
     return C;
-    
+
 }
 int* vSub(int *A,int *B,int size)
 {
